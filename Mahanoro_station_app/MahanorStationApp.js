e5dc7d6b624ebdfa2a8b80de222b1cb6a10fe1d2
Peta Jeannie <peta.jea@onja.org>
2021-02-14 08:38:49 -0600

    started the project by installint npm package, html file, index files and some folders to handle all the functions needed

---
+++ Mahanoro_station_app/MahanorStationApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MahanorStationApp() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default MahanorStationApp
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MahanorStationApp from "./Mahanoro_station_app/MahanorStationApp";
+
+ReactDOM.render(<MahanorStationApp/>, document.getElementById("root"));
